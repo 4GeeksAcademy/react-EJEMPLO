012bca4a178bcbb3981d0bff0a0d23db0a8bba0b
Patricia Alvarez <168550797+PatriciaAlEs@users.noreply.github.com>
2025-04-10 08:22:46 +0000

    tarjeta de mago API

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { TarjetaDeMago } from "./TarjetaDeMago.jsx";
 
-import {ListaDeLaCompra} from "./ListaDeLaCompra.jsx"
+
 
 //create your first component
 const Home = () => {
 	return (
 		<div className="text-center">
             <TarjetaDeMago/>
-			<ListaDeLaCompra/>
+
 		</div>
 	);
 };
--- src/js/components/TarjetaDeMago.jsx
+++ src/js/components/TarjetaDeMago.jsx
@@ -2,24 +2,17 @@ import React from "react";
 
 
 export const TarjetaDeMago = () => {
-    
+
 
 
     return (
         <>
-            {/* <div className="card">
-                <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiFq-osZFY-XVZhRyzrpChX_QK5JyhxinXiMtYuKzHoyYklAZPdwnQN9ufRGVAIMbbyV5YmEHQbEBWw7UsJ1PgsbF_U22fkEPKeoyHrxxjttBJZNF1m-LqKG9WN_V4fMd-w6rPXPn2ulCY/s1600/Sombrero+seleccionador+acuarela.JPG" className="card-img-top" alt="Mago" />
+            <div className="card">
                 <div className="card-body">
-                    <h5 className="nombre-mago">Nombre:</h5>
-                    <p className="casa-mago">Casa:</p>
-                    <p className="año-mago">Año Escolar:</p>
-                    <p className="hechizo-mago">Hechizo:</p>
-                    <button className="btn registro-mago btn-primary">
-                        Registrar Alumno
-                    </button>
+                    <img src="" alt="" className="card-img-top"/>
+                    <h5 className=""> Nombre del Mago: </h5>
                 </div>
-            </div> */}
-
+            </div>
         </>
     )
 }
