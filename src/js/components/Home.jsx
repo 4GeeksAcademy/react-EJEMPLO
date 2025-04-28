676ef6e38f59c81026fdd7cb6407d80fccb3d076
Patricia Alvarez <168550797+PatriciaAlEs@users.noreply.github.com>
2025-04-28 08:31:46 +0000

    ejercicos terminados

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { ListaDeLaCompra } from "./ListaDeLaCompra";
 
 
 
@@ -10,6 +11,7 @@ import React from "react";
 const Home = () => {
 	return (
 		<div className="text-center">
+            <ListaDeLaCompra/>
 
 		</div>
 	);
