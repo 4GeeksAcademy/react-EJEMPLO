5a39c7ac2aaddfb74918ab83ee35031c90e98075
Patricia Alvarez <168550797+PatriciaAlEs@users.noreply.github.com>
2025-05-15 07:33:52 +0000

    solo codigo

--- src/js/components/ListaDeLaCompra.jsx
+++ src/js/components/ListaDeLaCompra.jsx
@@ -1,68 +1,25 @@
-import React, { useEffect, useState } from "react";
-
-
+import React from "react"
 
 export const ListaDeLaCompra = () => {
-
-    // Estado para el producto
-    // Estado para la lista
-    // Input CONTROLADO (el dato que añadimos a traves del input)
-    // Añadir a la lista
-    // Mostrar los productos
-    
-    
-    const [producto, setProducto] = useState("")
-    const [lista, setLista] = useState([])
-
-    const añadirProducto = (nuevoProducto) => {
-        setProducto(nuevoProducto.target.value)
-    }
-
-
-    const actualizarLista = (e) => {
-        e.preventDefault()
-        setLista([...lista, producto])
-        setProducto("")
-    }
-
-    const eliminarProducto  = (e) => {
-        setLista(lista.filter((_,i) => i !== e))
-    }
-
-    useEffect(() => {
-        console.log(lista);
-        
-    }, [lista])
 
     return (
         <>
 
             <div className="container p-3 tex-center">
                 <h2>🛒 Mi lista de la compra</h2>
-                <form onSubmit={actualizarLista} className="d-flex gap-2 mb-3">
+                <form className="d-flex gap-2 mb-3">
                     <input
                         type="text"
                         placeholder="Producto"
-                        value={producto}
-                        onChange={añadirProducto}
                     />
-
                     <button type="submit">Agregar</button>
                 </form>
-
-
                 <ul className="list-group">
-                    { 
-                        lista.map((elemento, indice)=> 
-                           {return <li key={indice} className="list-group-item d-flex justify-content-between">
-                            {elemento}
-                            <span onClick={() => {eliminarProducto(indice)}}> ❌ </span>
-                        </li> }
-                        )
-                         
-                    }
-                       <p> {lista.length > 0 ? `Todo esto tienes que comprar: ${lista.length}` : `Añade productos a la lista`}  </p>
-               
+                    <li className="list-group-item d-flex justify-content-between">
+                        <span> ❌ </span>
+                    </li>
+                    <p>Añade productos a la lista </p>
+
                 </ul>
             </div>
 
